21a7bffad124bb62924e65ae869cc8d60a272e8d
Mahan Babelian <31671665+sleepywhiskey@users.noreply.github.com>
2022-05-10 09:22:05 +0430

    Create return_lastname.py

---
+++ return_lastname.py
@@ -0,0 +1,16 @@
+### this is a python file including 3 functions, find your function and return your last name
+###
+
+
+def hadis():
+  return 'no last name'
+
+
+
+def mahan():
+  return 'no last name'
+
+
+
+def sheida():
+return 'no last name'
